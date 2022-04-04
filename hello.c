004942dc540e8b2af0a6264811466c56ab7b9d84
lorenzrt <101933214+lorenzrt@users.noreply.github.com>
2022-04-04 17:11:28 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void){
+    char nombre [20];
+    printf ("Hola mequetrefe cara de albondiga, ingrese su nombre de ñoqui : I");
+    scanf ("%s", nombre);
+    printf ("Hola %s",nombre);
+    return 0;
+}
